f6bb68bd34639f392165ae0e1db56986bce7a594
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2023-11-21 15:15:23 +0000

    Using Process stdin to read input from a user

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,14 @@
+// Using Process stdin to read input from a user
+
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('data', (data) => {
+  const name = data.toString().trim();
+  console.log(`Your name is: ${name}`);
+  process.stdin.pause();
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+  process.exit(0);
+});
