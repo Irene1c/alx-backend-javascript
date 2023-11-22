aefc6c644bb929b405d5925ab6f7e772d980de2e
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2023-11-22 10:31:38 +0000

    Correct output

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,8 +3,8 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-  process.stdout.write(`Your name is: ${name}\r`);
+  const name = data.toString();
+  process.stdout.write(`Your name is: ${name}`);
   process.stdin.pause();
 });
 
