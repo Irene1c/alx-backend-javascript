2ef64d8d3ae4a0f292af111ae12214f683b541bc
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2023-11-24 15:52:51 +0000

    Promise handlers

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,10 @@
+// promise handlers
+
+export default function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => ({ status: 200, body: 'Success' }))
+    .catch(() => new Error())
+    .finally(() => {
+      console.log('Got a response from the API');
+    });
+}
